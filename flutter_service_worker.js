464234b649b56afd8536acabea0fcd498732edba
Jason Hawgood <jason@seavistasoftware.com>
2024-06-03 11:04:26 -0400

    Deploy updated Flutter web app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "3d76919590f5f03ee91dd91a2241916e",
-"index.html": "b5c19b87f6afcff09705946a2ac983bb",
-"/": "b5c19b87f6afcff09705946a2ac983bb",
-"main.dart.js": "b6db238009e5df1f2755558184a6f6f0",
+"index.html": "f3763114cb6c84b4f83bca6fe7e14355",
+"/": "f3763114cb6c84b4f83bca6fe7e14355",
+"main.dart.js": "adc90ff70156a4443f8f20b86169c718",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
