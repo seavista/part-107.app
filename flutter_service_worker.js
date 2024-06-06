9bc4cacf23433a5f4f4da03ca3658792bd23bbee
Jason Hawgood <jason@seavistasoftware.com>
2024-06-06 09:01:30 -0400

    Publishing web output to gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "3d76919590f5f03ee91dd91a2241916e",
-"index.html": "0811cc9711fa8421d287bd1d3de467d6",
-"/": "0811cc9711fa8421d287bd1d3de467d6",
-"main.dart.js": "adc90ff70156a4443f8f20b86169c718",
+"index.html": "6bbd55ff603bdfcb324483575e6c0460",
+"/": "6bbd55ff603bdfcb324483575e6c0460",
+"main.dart.js": "1d973c0e042ea496716308aa6855505d",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
