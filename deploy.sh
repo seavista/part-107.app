05420c3d3e16f536e582c90620ebb2b9e1dc0005
Jason Hawgood <jason@seavistasoftware.com>
2024-06-10 16:27:57 -0400

    Publishing web output to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Build the web app
+git checkout main
+flutter build web
+mkdir -p temp_build
+cp -r build/web/* temp_build/
+
+# Deploy to gh-pages
+git checkout gh-pages --force
+git reset --hard origin/gh-pages
+cp -rf temp_build/* .
+git add .
+git commit -m "Publishing web output to gh-pages"
+git push -u origin gh-pages --force
+
+# Switch back to main branch
+git checkout main
